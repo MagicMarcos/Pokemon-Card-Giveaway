f1e4acd925fa712c1792b261960eb8e1b43fd135
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-04-18 18:21:39 -0400

    minor server changes

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
         db = client.db(dbName)
     })
     
+    
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
